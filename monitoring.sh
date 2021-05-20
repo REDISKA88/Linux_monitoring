56a0c2dcdc6c38939b8349a3307c1efa87fcbcf0
VIRUSLOKI <virusloki@ya.ru>
2021-05-20 18:00:12 +0300

    dsa

--- monitoring.sh
+++ monitoring.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-ram=$(free -m | awk 'NR==2{printf"RAM: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
-wall -n " $ram"
+ram=$(free -m | awk 'NR==2{printf"%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
+hdd=$(df -h | awk '$NF=="/"{printf"%d/%dGB   (%s)\n", $3, $2, $5}')
+cpuload=$( 
+wall -n "RAM usage: $ram 
+HDD usage: $hdd
+CPU  load: $cpuload 
+"
 
